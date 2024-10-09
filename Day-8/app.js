d8140843a5677ac147e2407cb5a1ff3f271f4d35
chaitusoundikaivu <chaitu.gkchaitanya@gmail.com>
2024-10-09 12:36:07 +0530

    assigment created

---
+++ Day-8/app.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("button")
+console.log(btn);
+
+btn.addEventListener("click" , () => {
+    let h1 = document.querySelector("h1")
+    console.log(h1);
+    h1.style.color = "red"
+    h1.style.backgroundColor = "green"
+    
+})
